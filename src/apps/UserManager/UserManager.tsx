4dd8a005b55c5b1e0865a0754f7a87592f4593f7
PneumaLink <chfhr0202@gmail.com>
2025-03-19 21:41:43 +0900

    아주 간단한 추첨 프로그램 개발

--- src/apps/UserManager/UserManager.tsx
+++ src/apps/UserManager/UserManager.tsx
@@ -7,6 +7,8 @@ import UserCard from "./components/UserCard/UserCard";
 
 const UserManager = () => {
     const [userList, setUserList] = useState<User_Interface[]>([]);
+
+    const [selectedUser, setSelectedUser] = useState<User_Interface | null>(null);
     
     const addUserHandler = (name: string) => {
         setUserList((prev) => {
@@ -39,6 +41,22 @@ const UserManager = () => {
             setUserList(m_copy_list);
         }
     }
+
+    const selectHander = () => {
+        if(!window.confirm("추첨을 시작하시겠습니까?")){
+            return ;
+        }
+
+        // 무작위 인덱스 선택
+        const m_randomIndex = Math.floor(Math.random() * userList.length);
+
+        // 선택된 항목
+        const m_randomItem = userList[m_randomIndex];
+
+        console.log(m_randomItem);
+
+        setSelectedUser(m_randomItem);
+    }
     
     return(
         <Main>
@@ -56,6 +74,15 @@ const UserManager = () => {
                         />
                     )
                 })
+            }
+
+            <button onClick={selectHander}>추첨시작</button>
+
+            {
+                selectedUser
+                &&  <UserCard 
+                        name={selectedUser.name}
+                    />
             }
         </Main>
     )
